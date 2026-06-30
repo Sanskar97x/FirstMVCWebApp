952801cb70d55d0cf15004ef34fa2e7d3e35d610
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-30 18:30:39 +0545

    UserDTO added

---
+++ FirstMVCWebApp/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstMVCWebApp.Dto
+{
+    public class UserDto
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+    }
+}
