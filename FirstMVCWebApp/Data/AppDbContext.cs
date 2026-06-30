4cae5be5b53ac12181a38b32065e0d939e7f1463
Sanskar97x <tamangsanskar3@gmail.com>
2026-06-30 18:28:55 +0545

    AppDbContext Added

---
+++ FirstMVCWebApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstMVCApp.Models;
+
+namespace FirstMVCWebApp.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
